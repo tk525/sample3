e15bb241db226f57e0a51fffa5b2a261d4aa3b9e
滝口結 <[525tkg@gmail.com]>
2021-02-26 08:03:36 +0900

    no message

--- config.py
+++ config.py
@@ -1,12 +1,23 @@
 from configparser import ConfigParser
 import os
 
+def db():
 
-def config(filename='heroku.ini', section='postgresql'):
-    # create a parser
+    host = os.environ['DB_HOSTNAME']
+    database = os.environ['DB_NAME']
+    user = os.environ['DB_USERNAME']
+    password = os.environ['DB_PASSWORD']
+    port=5432
+    return host, database, user, password, port
+
+
+# def config(filename='heroku.ini', section='postgresql'):
+def config(section='postgresql'):
+
     parser = ConfigParser()
-    # read config file
-    parser.read(filename)
+
+    # parser.read(filename)
+    parser.read(db)
 
     # get section, default to postgresql
     db = {}
@@ -15,6 +26,7 @@ def config(filename='heroku.ini', section='postgresql'):
         for param in params:
             db[param[0]] = param[1]
     else:
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+        # raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+        raise Exception('Section {0} not found in the {1} file'.format(section, db))
 
     return db
