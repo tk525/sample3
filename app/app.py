ba389c3fb4948e2e6f6c1e378739f9b30567399e
滝口結 <[525tkg@gmail.com]>
2021-03-05 08:23:08 +0900

    no message

--- app/app.py
+++ app/app.py
@@ -530,7 +530,7 @@ def twmc_ajax():
 SECRET_KEY = os.environ.get('SECRET_KEY')
 app.config['SECRET_KEY'] = SECRET_KEY
 # socketio = SocketIO(app, cors_allowed_origins='*')
-socketio = SocketIO(app, async_mode=None)
+# socketio = SocketIO(app, async_mode=None)
 
 # result = q.enqueue(background_process, socketio)
 
