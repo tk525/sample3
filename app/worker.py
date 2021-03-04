2fdbefd0646c01691681f7d59e4ebf49e9b0360c
滝口結 <[525tkg@gmail.com]>
2021-03-04 14:43:14 +0900

    no message

--- app/worker.py
+++ app/worker.py
@@ -5,11 +5,11 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-# redis_url = os.getenv('REDISTOGO_URL', 'https://sample301-20210223.herokuapp.com/twmc_p')
-DATABASE_URL = os.environ.get('DATABASE_URL')
+redis_url = os.getenv('REDISTOGO_URL', 'redis://sample301-20210223.herokuapp.com/twmc_p')
+# DATABASE_URL = os.environ.get('DATABASE_URL')
 
-# conn = redis.from_url(redis_url)
-conn = redis.from_url('https://sample301-20210223.herokuapp.com/twmc_p', db=DATABASE_URL)
+conn = redis.from_url(redis_url)
+# conn = redis.from_url('https://sample301-20210223.herokuapp.com/twmc_p', db=DATABASE_URL)
 
 if __name__ == '__main__':
     with Connection(conn):
